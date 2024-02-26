83e837a8153df75d6f8ee172ea676fb816ad3fd3
eduardoDahmer1 <eduluizdahmer@gmail.com>
2024-02-26 15:25:29 -0300

    commit

---
+++ loop-lista.php
@@ -0,0 +1,7 @@
+<?php
+// comando para rodar o servidor embutido php php -S localhost:8080
+$idadeList = [23, 19, 25, 30, 41, 18, 21, 35];
+
+for ($i = 0; $i < count($idadeList); $i++) {
+    echo $idadeList[$i] . PHP_EOL;
+}
---
+++ tipos-chaves.php
@@ -0,0 +1,12 @@
+<?php
+// comando para rodar o servidor embutido php php -S localhost:8080
+$array = [
+    1 => 'a',
+    '1' => 'b',
+    1.5 => 'c',
+    true => 'd'
+];
+
+foreach ($array as $item) {
+    echo $item . PHP_EOL;
+}
